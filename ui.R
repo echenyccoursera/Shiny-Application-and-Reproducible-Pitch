d6f40771847044239749c8dd915b017f268bbf79
echenyccoursera <67453530+echenyccoursera@users.noreply.github.com>
2020-09-05 00:37:07 -0500

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,42 +1,25 @@
-library(leaflet)
-library(shiny)
-shinyUI(fluidPage( 
+# ui.R
+ui <- fluidPage(
   
-  tabsetPanel(
-    tabPanel("MapApp", fluid = TRUE,
+  # App title ----
+  titlePanel("Shiny"),
   
-  titlePanel("2017 Car crash data in Allegheny County"),
+  # Sidebar 
   sidebarLayout(
+    
     sidebarPanel(
-#Slider to slice based on a month (or select all)
-      selectInput(
-        inputId =  "Month", 
-        label = "Select Month:", 
-        choices = c("All",month.name),
-        selected = "All"
-      ),
       
-#Create the slider for selecting the number of cars involved 
-#The slider is a dynamic slider, as the maximum and minuim values 
-# are based on the data 
-      sliderInput("slider", "Select Number of Cars involve (Range)", min = 1,
-                    max = 10, value = 1,step= 1),
-      
-      
-#Submit button - control the changes       
-      submitButton("Submit")
+      sliderInput(inputId = "bins",
+                  label = "Number of bins",
+                  min = 1,
+                  max = 30,
+                  value = 5)
       
     ),
-#Main Panel 
+    # Main panel  
     mainPanel(
-#Render the MAP
-      leafletOutput("map", width = "90%", height = "700px")
+      plotOutput(outputId = "distPlot")
     )
   )
-),
-tabPanel("Help", fluid = TRUE,
-         
-         tags$img(src='Help.png',width="700px",height="700px")
+)
 
-         
-))))
